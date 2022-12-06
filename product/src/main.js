98be35f54698c6ce2e01cf34b285a7df96080230
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2022-12-06 11:55:50 -0300

    Incluindo comentario
    Incluindo comentário no código.

--- product/src/main.js
+++ product/src/main.js
@@ -1,1 +1,5 @@
+// fazendo log da mensagem: "Inigit ciando product"
+//Usar metodo console.log 
 console.log('Iniciando product');
+
+//Executar mensagem no terminal: dentro da pasta product\src digitar o comando <node main.js>
