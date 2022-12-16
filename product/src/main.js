4de96cce856640e26c70bcf67cf8600831981b6c
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2022-12-16 17:37:07 -0300

    inicialize dentro dele uma aplicação express
    Iniciando uma aplicação express que deve ouve por requisições na porta 3000.

--- product/src/main.js
+++ product/src/main.js
@@ -1,5 +1,20 @@
-// fazendo log da mensagem: "Inigit ciando product"
-//Usar metodo console.log 
-console.log('Iniciando product');
+import express, { json } from 'express';
+import { router } from './routes.js';
 
-//Executar mensagem no terminal: dentro da pasta product\src digitar o comando <node main.js>
+const port = 3000;
+const app = express();
+
+app.use(json());
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`Servidor de contas em execução http://localhost:${port}`)
+  });
+
+/*
+fazendo log da mensagem: "Inigit ciando product"
+Usar metodo console.log 
+*console.log('Iniciando product');
+
+Executar mensagem no terminal: dentro da pasta product\src digitar o comando <node main.js>
+*/
