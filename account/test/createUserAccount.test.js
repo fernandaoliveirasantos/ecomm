1c6de11a03a45e6609e7bdf62f373e6238a42951
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2022-12-22 16:29:07 -0300

    Alterando arquivo de pasta.
    Arquivo incluído na pasta use-case.

--- account/test/createUserAccount.test.js
+++
@@ -1,6 +0,0 @@
-import { createUserUseCase } from '../../src/use-case/createUserAccount.js';
-
-const user1 = await createUserUseCase('Fernanda', 'fernanda@example.com', 'mudarsenha123');
-const user2 = await createUserUseCase('Josué', 'josue@example.com', 'senha123');
-
-console.log(user1, user2);
