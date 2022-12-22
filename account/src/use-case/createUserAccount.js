c87ad874cbabc0590abab144c90fb12a45d9fe61
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2022-12-22 16:22:30 -0300

    Refatorando codigo

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -1,22 +1,15 @@
-import { saveAccount, findAccountByEmail } from '../repositories/accountRepository.js';
+import {saveAccount} from '../repositories/accountRepository.js';
 
 export async function createUserUseCase(name, email, password) {
-
-    const accountAlreadyExists = await findAccountByEmail(email);
-
-    if (accountAlreadyExists !== null) {
-        console.error('Account already exists', email);
-        return;
-    }
-    
     const createdDate = new Date().toISOString().substring(0, 10);
+    
     const user = {
         name,
         email,
         password,
-        createdDate
-    }
-    saveAccount(user);
+        createdDate,
+    };
 
-    return user
+    await saveAccount(user);
+    return user;
 }
