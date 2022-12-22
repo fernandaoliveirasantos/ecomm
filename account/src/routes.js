ccfe021b6aa0ed1f874051fa553b1abba853770b
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2022-12-22 16:24:38 -0300

    Refatorando codigo

--- account/src/routes.js
+++ account/src/routes.js
@@ -1,36 +1,20 @@
-import { Router } from 'express';
-import { createUserUseCase } from "../src/use-case/createUserAccount.js";
-import bcrypt from 'bcryptjs';
+import { Router } from "express";
+
+import { createUserUseCase } from "./use-case/createUserAccount.js";
+
+
 const router = Router();
 
-function encodePass(password) {
-    const saltRounds = 10;
-    const hash = bcrypt.hashSync(password, saltRounds);
-    return hash;
-}
+router.post('/accounts', async (request, response) => {
+    const { name, email, password } = request.body;
+    const createdUser = await createUserUseCase(name, email, password);
 
-router.get('/', (request, response) => {
-    res.send('Accounts')
+    return response.status(201).json({
+        id: createdUser._id,
+        name: createdUser.name,
+        email: createdUser.email,
+        createdDate: createdUser.createdDate,
+    });
 });
 
-router.post('/accounts', function (request, response) {
-
-    const { name, email, password } = request.body
-
-    const pass = encodePass(password)
-   
-    createUserUseCase(name, email, pass)
-        .then((data) => {
-            console.log(data)
-            let user = {
-                name: data.name,
-                email: data.email,
-                date: data.createdDate
-            }
-
-            request.status(201).json(user);
-        })
-        .catch((error) => {
-            response.status(400).json({ status: 'Error creating user!', message: error.message });
-        })
-});
+export { router };
