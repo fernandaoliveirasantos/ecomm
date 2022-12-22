a7ce6a46516fa5be7168fe9537d078bcd837321a
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2022-12-22 16:20:28 -0300

    Refatorando código

--- account/src/repositories/accountRepository.js
+++ account/src/repositories/accountRepository.js
@@ -1,40 +1,19 @@
-import {MongoClient} from 'mongodb';
+import { MongoClient } from "mongodb";
 
-async function getUsersCollection() {
-    const connectionURL = 'mongodb://mongouser:mongopass@account_db:27017'; 
-    const connection = new MongoClient(connectionURL); 
-    await connection.connect();
+const client = new MongoClient('mongodb://mongouser:mongopass@localhost:27017');
 
-    const database = connection.db('accounts');
-    return database.collection('users')
+async function getUsersCollection(client) {
+    const db = client.db('accounts');
+    const usersCollection = db.collection('users');
+    return usersCollection;  
 }
 
-export async function saveAccount (account) {
-    try {
-    const collection = await getUsersCollection();
-    await collection.insertOne(account);
-} catch (e) {
-    console.error("unsaved account =======", e.message.stack);
-} finally {
-    await connectionClosed()
+export async function saveAccount(account) {
+    await client.connect();
+    const usersCollection = await getUsersCollection(client);
+    await usersCollection.insertOne(account);
+    await client.close();
 }
-}
-
-export async function findAccountByEmail(email) {
-try {
-    const collection = await getUsersCollection();
-    const account = await collection.findOne({ email });
-    return account;
-} catch (e) {
-    console.error("error fetching email:=======", e.message.stack)  
-}
-}
-
-async function connectionClosed() {
-connection.close();
-console.log("connection closed =======")
-}
-
 //async function getUsersCollection() { // Salva a conta no mongodb (valor de retorno da função será, "por baixo dos panos")
 
 //const connection = new MongoClient(conectionURL); //faz a execução de uma função async pausar, para esperar pelo retorno da Promise , e resume a execução da função async quando o valor da Promise é resolvido.
