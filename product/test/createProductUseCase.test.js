8bcffc907bacb9f008a68bb8ba85eeb454337187
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2022-12-22 16:41:19 -0300

    Arquivo add na pasta use-case

--- product/test/createProductUseCase.test.js
+++
@@ -1,7 +0,0 @@
-import { createProductUseCase } from "../src/use-case/createProductUseCase.js";
-
-import { produto } from "./products.js";
-
-const product = await createProductUseCase(produto);
-
-console.log("Produto: ", product)
