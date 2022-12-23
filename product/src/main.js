dedf9b4fa80072071fe483480e979a43671d0099
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2022-12-23 16:05:36 -0300

    Add. imports
    -cors
    -swaggerExpress
    -swaggerDocs from

--- product/src/main.js
+++ product/src/main.js
@@ -1,17 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import swaggerExpress from 'express-swagger-ui-express';
+import swaggerDocs from '../api-docs.json' assert { type: 'json'};
 
 import { router } from './routes.js';
+import { assert } from 'console';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use('/api-docs', swaggerExpress.serv, swaggerExpress.setup(swaggerDocs));
 
 app.get('/health', (request, response) => {
     return response.send();
-});
-
-app.use(router);
-
-app.listen(3000, () => {
-    console.log('products service is running');
 });
