1dd5c0b054fbda58693981e3e8715614d23155cf
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2022-12-23 15:56:53 -0300

    Adicionando imports
    -cors
    -swaggerExpress 
    -swaggerDocs

--- account/src/main.js
+++ account/src/main.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import swaggerExpress from 'express-swagger-ui-express';
+import swaggerDocs from '../api-docs.json' assert { type: 'json'};
 
 import { router } from './routes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use('api-docs', swaggerExpress.serv, swaggerExpress.setup(swaggerDocs));
 
 app.get('/health', (request, response) => {
     return response.send();
