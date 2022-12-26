18fdd97fceec3f78710a9c4a88e7821cb3601d69
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2022-12-26 10:07:05 -0300

    Update main.js

--- account/src/main.js
+++ account/src/main.js
@@ -5,11 +5,12 @@ import swaggerDocs from '../api-docs.json' assert { type: 'json'};
 
 import { router } from './routes.js';
 
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('api-docs', swaggerExpress.serve, swaggerExpress.setup(swaggerDocs));
+app.use('/api-docs', swaggerExpress.serve, swaggerExpress.setup(swaggerDocs));
 
 app.get('/health', (request, response) => {
     return response.send();
