8791e8212f3dd0e85e9e75c28720e4fc63ca23c4
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2023-01-10 16:09:49 -0300

    Refatorando código

--- product/src/repositories/productRepository.js
+++ product/src/repositories/productRepository.js
@@ -1,12 +1,28 @@
-
-import { Product } from '../../api/models/product.js'
+import { Product } from '../../models/product.js'
+import { ProductFeature } from '../../models/product_feature.js';
+import { ProductImage } from '../../models/product_image.js';
 
 export async function saveProduct(product) {
-    const createdProduct = await Product.create(product)
-    await createdProduct.save()
+    const createdProduct = await Product.create(product, {
+        include: [
+            { association: Product.ProductFeature, as: 'features',  },
+            { association: Product.ProductImage, as: 'images' }
+        ]
+    });
+    await createdProduct.save();
+    
     return createdProduct;
 }
 
 export async function findProducts() {
-    return [];
+    const products = await Product.findAll({ include: [
+        {
+            model: ProductFeature,
+            as: 'features'
+        }, {
+            model: ProductImage,
+            as: 'images'
+        }] 
+    });
+    return products;
 }
