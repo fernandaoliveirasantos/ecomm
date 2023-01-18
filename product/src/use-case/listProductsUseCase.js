bd676edb263dec7a76c2102ed1b7edd552d9b8b8
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2023-01-18 14:08:00 -0300

    Validando console.log

--- product/src/use-case/listProductsUseCase.js
+++ product/src/use-case/listProductsUseCase.js
@@ -2,5 +2,6 @@ import { findProducts } from "../repositories/productRepository.js";
 
 export async function listProductsUseCase() {
     const product = await findProducts();
+    console.log(product)
     return product;
 }
