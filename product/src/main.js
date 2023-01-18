3c2210a80fe68b1bf2e99fb06f9c9129343dd3fd
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2023-01-18 14:10:04 -0300

    Refatorando o código

--- product/src/main.js
+++ product/src/main.js
@@ -1,37 +1,17 @@
-import express from 'express';
-import cors from 'cors';
 import swaggerExpress from 'swagger-ui-express';
 import yaml from 'yamljs';
-import { router } from './routes.js';
 
+import { app } from './app.js'
 import client from './repositories/databaseClient.js';
 
 const swaggerDocs = yaml.load('./docs.yaml');
 
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-app.get('/health', (request, response) => {
-    return response.send();
-});
-
-app.use(router);
-app.use('/api-docs', swaggerExpress.serve, swaggerExpress.setup(swaggerDocs));
-
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+app.use('/docs', swaggerExpress.serve, swaggerExpress.setup(swaggerDocs));
 
 app.listen(3000, () => {
-    console.log('products service is running');
+    console.log('Serviço de produtos em execução');
 
     client.authenticate()
-      .then(() => console.log('conectando com mysql'))
-      .catch(error => console.error(error.message));
-    
+        .then(() => { console.log('banco de dados conectado') })
+        .catch(error => { console.error(error)});
 });
