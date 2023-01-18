6da0200ff0189c49ee0ec6e900dcf3d886541d76
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2023-01-18 13:48:41 -0300

    Refatorando código

--- account/src/main.js
+++ account/src/main.js
@@ -1,20 +1,12 @@
-import express from 'express';
 import yamljs from 'yamljs';
 import swaggerExpress from 'swagger-ui-express';
-import { router } from './routes.js';
+
+import { app } from './app.js';
 
 const swaggerDocs = yamljs.load('./docs.yaml');
-const app = express();
-app.use(express.json());
 
-app.get('/health', (request, response) => {
-    return response.send();
-});
+app.use('/docs', swaggerExpress.serve, swaggerExpress.setup(swaggerDocs));
 
-app.use(router);
-
-app.use('/docs', swaggerExpress.serve, swaggerExpress.setup(swaggerDocs))
-
-app.listen(3001, () => {
+app.listen(3000, () => {
     console.log('Serviços de contas em execução');
 });
