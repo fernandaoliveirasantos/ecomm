a38aee74e4dade5112a320696dd48539de2efaaf
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2023-01-18 13:53:02 -0300

    Refatorando codigo

--- account/test/use-case/createUserAccount.test.js
+++ account/test/use-case/createUserAccount.test.js
@@ -1,6 +1,34 @@
-import { createUserUseCase } from '../../src/use-case/createUserAccount.js';
+import request from 'supertest';
 
-const user1 = await createUserUseCase('Fernanda', 'fernanda@example.com', 'mudarsenha123');
-const user2 = await createUserUseCase('Josué', 'josue@example.com', 'senha123');
+import { app } from '../../src/app.js';
+import { client, getUsersCollection } from '../../src/repositories/accountRepository.js';
 
-console.log(user1, user2);
+describe('Criação de conta', () => {
+    afterEach(async () => {
+        await client.connect();
+        const usersCollection = await getUsersCollection(client);
+        await usersCollection.deleteMany({});
+        await client.close();
+    });
+
+    it('Cadastrar um usuário se os dados estiverem corretos', async () => {
+        await request(app)
+            .post('/accounts')
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .send({
+                name: 'Nicolas',
+                email: 'nicolas@email.com',
+                password: 'senhamudar@'
+            })
+            .expect(201)
+            .expect(({ body }) => {
+                expect(body).toEqual({
+                    id: expect.any(String),
+                    name: 'Nicolas',
+                    email: 'nicolas@email.com',
+                    createdDate: new Date().toISOString().slice(0, 10)
+                })
+            });
+    });
+});
