968b90e3b1dfd0444eee30e38c793b36ff018233
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2023-01-24 17:04:38 -0300

    Atualizando routes.js

--- product/src/routes.js
+++ product/src/routes.js
@@ -1,13 +1,32 @@
 import { Router } from 'express';
-
 import { createProductUseCase } from './use-case/createProductUseCase.js';
 import { listProductsUseCase } from './use-case/listProductsUseCase.js';
+import { decriptToken } from './helpers/token.js';
 
 const router = Router();
 
 router.post('/products', async (request, response) => {
+    const authorizationHeader = request.headers.authorization;
+    if(!authorizationHeader) {
+        return response.status(401).json({ message: 'Acesso negado. Nenhum token fornecido' })
+    }
+    
+    const token = authorizationHeader.split(' ')[1];
+    
+    if(!token) {
+        return response.status(400).json({ message: 'cabeçalho de autorização mal-formado'});
+    }
+    
+    
+    const tokenDecripted = decriptToken(token);
+    const userId = tokenDecripted.userId;
+
+    if(!userId) {
+        return response.status(403).json({ message: 'proíbido' });
+    }
+
     const product = request.body;
-    const createdProduct = await createProductUseCase(product);
+    const createdProduct = await createProductUseCase(product, userId);
 
     return response.status(201).json(createdProduct);
 });
