85c9299f543c688ba93eb98d3f5d02b853b1e3c7
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2023-01-24 17:07:37 -0300

    Refatorando createProduct.test.js

--- product/test/e2e/createProduct.test.js
+++ product/test/e2e/createProduct.test.js
@@ -3,13 +3,8 @@ import { Product } from '../../models/product.js';
 import {app} from  '../../src/app.js';
 import { findProducts } from '../../src/repositories/productRepository.js';
 import { productExample } from '../data/products.js';
-import { cleanProductTable } from '../helpers/product.js';
 
 describe('Cria Produto', () => {
-
-    afterEach(async () => {
-        await cleanProductTable();
-    });
 
     it('Cadastrar um produto se os dados estiverem corretos', async () => {
         await request(app)
