49eb0dec25b1e4f45a652f1efd4597d2d0b40145
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2023-02-03 18:16:59 -0300

    Aletrando porta

--- account/src/main.js
+++ account/src/main.js
@@ -1,12 +1,12 @@
 import yamljs from 'yamljs';
 import swaggerExpress from 'swagger-ui-express';
-
 import { app } from './app.js';
 
+const PORT = process.env.PORT;
 const swaggerDocs = yamljs.load('./docs.yaml');
 
 app.use('/docs', swaggerExpress.serve, swaggerExpress.setup(swaggerDocs));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Serviços de contas em execução');
 });
