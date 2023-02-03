ff6a8300a595134a90b1e6ece713b2a5c8c0013c
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2023-02-03 18:52:17 -0300

    Update main.js

--- product/src/main.js
+++ product/src/main.js
@@ -4,11 +4,12 @@ import yaml from 'yamljs';
 import { app } from './app.js'
 import client from './repositories/databaseClient.js';
 
-const swaggerDocs = yaml.load('./docs.yaml');
+//const swaggerDocs = yaml.load('./docs.yaml');
+const PORT = process.env.PORT;
 
-app.use('/docs', swaggerExpress.serve, swaggerExpress.setup(swaggerDocs));
+//app.use('/docs', swaggerExpress.serve, swaggerExpress.setup(swaggerDocs));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Serviço de produtos em execução');
 
     client.authenticate()
